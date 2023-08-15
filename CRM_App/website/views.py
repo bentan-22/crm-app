e1a0854380207a63cd1f5fddec13f476876750de
Ben Tan <benjaminthtan@gmail.com>
2023-08-15 16:40:17 +0800

    add register

--- CRM_App/website/views.py
+++ CRM_App/website/views.py
@@ -14,14 +14,21 @@ def home(request):
         if user is not None:
             login(request, user)
             messages.success(request, "You have successfully logged in.")
-            return redirect('home')
+            return redirect("home")
         else:
-            messages.success(request, "There was an error logging in. Please try again.")
-            return redirect('home')
+            messages.success(
+                request, "There was an error logging in. Please try again."
+            )
+            return redirect("home")
     else:
         return render(request, "home.html", {})
+
 
 def logout_user(request):
     logout(request)
     messages.success(request, "You have successfully logged out.")
-    return redirect('home')
+    return redirect("home")
+
+
+def register_user(request):
+    return render(request, "register.html", {})
